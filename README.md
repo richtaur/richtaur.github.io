6794c45d52f5f8f82decf9f3f0d8616fb2e3ed45
richtaur <richtaur@gmail.com>
2010-07-10 23:03:40 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# richtaur.com
+
+Just the little website at richtaur.com, used as Matt Hackett's personal site. Used to be a Wordpress blog (hence some of the weird markup etc.).
---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html class="day" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>richtaur</title>
+	<link rel="index" title="richtaur" href="http://richtaur.com/">
+	<link rel="icon" href="/favicon.gif" type="image/ico">
+	<link rel="openid.delegate" href="http://richtaur.myopenid.com/">
+	<link rel="openid.server" href="http://www.myopenid.com/server">
+	<link rel="shortcut icon" href="/favicon.gif">
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/combo?2.7.0/build/reset/reset-min.css">
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="description" content="Stuff about richtaur/Matt Hackett">
+	<meta name="keywords" content="richtaur, matt hackett, web developer, silicon valley, video games, foosball, sharks, yahoo, raptr, lost decade games, retro, onslaught!">
+	<meta name="title" content="richtaur">
+</head>
+<body>
+
+<div class="con" id="hd">
+	<h1>
+		<a href="/" title="Now you're playing with richtaur">richtaur</a>
+	</h1>
+	<div id="avatar">
+		<div></div>
+	</div>
+</div><!-- /#hd -->
+
+<div id="divider">
+	<div class="con">
+
+		<ul class="btns">
+			<li><button class="close" tooltip="Browser slowing down? Toggle all animations here"><span>close</span></button></li>
+			<li><button class="disabled"><span>TBD</span></button></li>
+			<li><button class="toggle" tooltip="Toggle ALL content on the page"><span>toggle content</span></button></li>
+			<!--li><button class="toggle" tooltip="Toggle ALL content on the page"><span>toggle content</span></button></li>
+			<li><button class="search" tooltip="Search for stuff"><span>search</span></button></li-->
+		</ul>
+
+	</div><!-- /.con -->
+</div><!-- /#divider -->
+
+<div class="con" id="bd">
+
+	<div id="column-primary">
+
+		<div class="module">
+			<div>
+				<div class="hd">
+					<ul class="btns">
+						<li><button class="close" tooltip="Close this module"><span>close</span></button></li>
+						<li><button class="toggle" tooltip="Toggle this module's contents"><span>toggle content</span></button></li>
+						<li><button class="maximize" tooltip="Maximize this module"><span>maximize</span></button></li>
+					</ul>
+					<h2>Welcome</h2>
+				</div>
+				<div class="bd">
+					<div>
+
+			
+<!-- begin content -->
+<div class="post"> 
+ 
+<p class="primary"> 
+	My name's Matt Hackett and this is my personal website. Here are some hopefully interesting things about me, in a descendingly less interesting order:
+</p> 
+ 
+<br/> 
+<img alt="Me playing foos" class="after" src="img/about_foos.jpg"> 
+ 
+<ol class="list-detailed"> 
+	<li> 
+		I've been a professional web developer since 2003, working for such companies as
+		<a href="http://schoolcenter.com/">SchoolCenter</a>,
+		<a href="http://www.yahoo.com/">Yahoo!</a> and
+		<a href="http://raptr.com/">Raptr</a>.
+	</li> 
+	<li> 
+		I was raised in Carterville, IL and
+		graduated from Southern Illinois University
+		with a Bachelor of Arts in Radio &amp; Television
+		and a minor in Marketing.
+	</li> 
+	<li>I contributed 60 seconds worth of music to Capcom's <a href="http://www.ocremix.org/info/OC_ReMix:_Super_Street_Fighter_II_Turbo_HD_Remix_Official_Soundtrack">Street Fighter 2 HD Remix</a></li> 
+	<li>I'm a <a href="http://www.flickr.com/photos/matthackett/3488395022/">San Jose foosball champion</a>.</li> 
+	<li>I've written <strong>one</strong> <a href="http://www.gamesradar.com/ds/the-dark-spire/review/the-dark-spire/a-20090518145357896028/g-20081218172959531054">professional game review</a>.</li> 
+	<li>I was an extra in a <a href="http://www.imdb.com/title/tt0297024/">really shitty movie</a>.</li> 
+	<li>I was born the day <a href="http://en.wikipedia.org/wiki/John_Lennon">John Lennon</a> died.</li> 
+</ol> 
+ 
+<p class="sep"> 
+	Feel free to contact me. To minimize spam, I put my email in a little flash here:
+	<object width="121" height="19"><param name="movie" value="/email.swf"></param><param name="allowFullScreen" value="false"></param><param name="allowscriptaccess" value="always"></param><embed src="/email.swf" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="121" height="19"></embed></object> 
+</p> 
+						</div><!-- /.post --> 
+<!-- end content -->
+	
+	
+	
+		</div></div>
+		<div><div>
+		
+	
+					</div>
+				</div><!-- /.bd -->
+			</div>
+		</div><!-- /.module (first in primary) -->
+
+
+	</div><!-- /#column-primary -->
+
+	<div id="column-secondary">
+
+		<!--div class="module">
+			<div>
+				<div class="hd">
+					<ul class="btns">
+						<li><button class="close" tooltip="Close this module"><span>close</span></button></li>
+						<li><button class="toggle" tooltip="Toggle this module's contents"><span>toggle content</span></button></li>
+						<li><button class="maximize" tooltip="Maximize this module"><span>maximize</span></button></li>
+					</ul>
+					<h2>Blogroll</h2>
+				</div>
+				<div class="bd">
+					<div>
+						<ul>
+							<li><a href="http://blog.360.yahoo.com/douglascrockford">Douglas Crockford</a></li>
+							<li><a href="http://www.gamesradar.com/">GamesRadar</a></li>
+							<li><a href="http://geoffblair.com/">Geoff Blair</a></li>
+							<li><a href="http://foohack.com/">Isaac Schlueter</a></li>
+							<li><a href="http://lostdecadegames.com/">Lost Decade Games</a></li>
+							<li><a href="http://blog.nihilogic.dk/">Nihilogic</a></li>
+							<li><a href="http://blog.raptr.com/">Raptr Blog</a></li>
+							<li><a href="http://scriptnode.com/">scriptNode</a></li>
+							<li><a href="http://yuiblog.com/">YUI Blog</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div--><!-- /.module -->
+
+		<div class="module" id="mod-twitter">
+			<div>
+				<div class="hd">
+					<ul class="btns">
+						<li><button class="close" tooltip="Close this module"><span>close</span></button></li>
+						<li><button class="toggle" tooltip="Toggle this module's contents"><span>toggle content</span></button></li>
+						<li><button class="maximize" tooltip="Maximize this module"><span>maximize</span></button></li>
+					</ul>
+					<h2>Twitter (<a href="http://twitter.com/richtaur" tooltip="Follow me on Twitter!">richtaur</a>)</h2>
+				</div>
+				<div class="bd">
+					<div>
+						<p>
+							<a class="img-con" href="http://twitter.com/richtaur"><img alt="Twitter" src="img/icons/twitter_100x100.png"></a>
+							<?php
+							$json = json_decode(file_get_contents('twitter.json'));
+							$desc = (string) $json->channel->item[0]->title;
+							echo substr($desc, 10);
+							?>
+						</p>
+						<p>
+							<a href="http://twitter.com/richtaur">Follow me on Twitter</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div><!-- /.module -->
+
+		<iframe
+			frameborder="0"
+			id="raptr-badge"
+			src="http://raptr.com/badges/last-most/richtaur"
+			scrolling="no"
+		></iframe>
+
+	</div><!-- /#column-secondary -->
+
+</div><!-- /#bd -->
+
+<div id="ft">
+	<div class="con">
+		<em>Original content &copy; 2010 Matt Hackett</em>
+	</div>
+</div>
+
+<div id="extra-1"></div>
+<div id="extra-2"></div>
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js"></script>
+<script type="text/javascript" src="richtaur.js"></script>
+<script type="text/javascript">
+richtaur.setPlaying(<?php echo file_get_contents('now_playing.json'); ?>);
+richtaur.setWeather(<?php echo file_get_contents('yweather.json'); ?>);
+</script>
+
+</body>
+</html>
