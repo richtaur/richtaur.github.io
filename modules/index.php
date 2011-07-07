31f4c45f8c200045910c5307751938ec9a560255
Matt Hackett <richtaur@gmail.com>
2011-07-07 03:20:12 -0700

    updating and preparing for a new skin maybe

---
+++ modules/index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html class="day" lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Hey guys it's Matt Hackett!</title>
+	<link rel="icon" href="img/favicon.gif" type="image/ico">
+	<link rel="openid.delegate" href="http://richtaur.myopenid.com/">
+	<link rel="openid.server" href="http://www.myopenid.com/server">
+	<link rel="shortcut icon" href="img/favicon.gif">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<meta name="description" content="Matt Hackett's website. He's an HTML5 game developer and stuff.">
+	<meta name="keywords" content="richtaur, matt hackett, game developer, silicon valley, video games, foosball, sharks, yahoo, raptr, lost decade games, retro, onslaught!, html5, javascript">
+</head>
+<body>
+
+<div class="con" id="hd">
+	<h1>
+		<a href="/" title="Now you're playing with richtaur">richtaur</a>
+	</h1>
+	<div id="avatar">
+		<div></div>
+	</div>
+</div>
+
+<div id="divider">
+	<div class="con">
+		<ul class="btns">
+			<li><button class="close" tooltip="Browser slowing down? Toggle all animations here"><span>close</span></button></li>
+			<li><button class="disabled"><span>TBD</span></button></li>
+			<li><button class="toggle" tooltip="Toggle ALL content on the page"><span>toggle content</span></button></li>
+		</ul>
+	</div>
+</div><!-- /#divider -->
+
+<div class="con" id="bd">
+
+	<div id="column-primary">
+
+		<div class="module">
+			<div>
+				<div class="hd">
+					<ul class="btns">
+						<li><button class="close" tooltip="Close this module"><span>close</span></button></li>
+						<li><button class="toggle" tooltip="Toggle this module's contents"><span>toggle content</span></button></li>
+						<li><button class="maximize" tooltip="Maximize this module"><span>maximize</span></button></li>
+					</ul>
+					<h2>Welcome</h2>
+				</div>
+				<div class="bd">
+					<div>
+
+<!-- begin content (copypasta from Word Press) -->
+<div class="post">
+
+<p class="primary">
+	My name's <strong>Matt Hackett</strong> and this is my personal website. Here are some hopefully interesting things about me, in a descendingly less interesting order:
+</p>
+
+<br>
+<img alt="Me playing foos" class="after" src="img/about_foos.jpg">
+
+<ol class="list-detailed">
+	<li>
+		I'm <strong>Studio Director</strong> at
+		<a href="http://www.lostdecadegames.com/">Lost Decade Games</a>,
+		where I work on <strong>HTML5 games</strong> fulltime.
+	</li>
+	<li>
+		I was a <strong>web developer</strong> from 2003 to 2011, working at places like
+		<a href="http://www.yahoo.com/">Yahoo!</a> and
+		<a href="http://raptr.com/">Raptr</a>.
+	</li>
+	<li>I contributed <strong>60 seconds worth of music</strong> to Capcom's <a href="http://www.ocremix.org/info/OC_ReMix:_Super_Street_Fighter_II_Turbo_HD_Remix_Official_Soundtrack">Street Fighter 2 HD Remix</a>.</li>
+	<li>I've written one <a href="http://www.gamesradar.com/ds/the-dark-spire/review/the-dark-spire/a-20090518145357896028/g-20081218172959531054">professional game review</a>.</li>
+	<li>I'm a <a href="http://www.flickr.com/photos/matthackett/3488395022/">San Jose foosball champion</a>.</li>
+	<li>I was an extra in a <a href="http://www.imdb.com/title/tt0297024/">really shitty movie</a>.</li>
+	<li>
+		I graduated from Southern Illinois University (USA)
+		with a <strong>Bachelor of Arts in Radio &amp; Television</strong>
+		and a minor in Marketing. Weird right?
+	</li>
+	<li>I was born the day <a href="http://en.wikipedia.org/wiki/John_Lennon">John Lennon</a> died.</li>
+</ol>
+
+<p class="primary">
+	This website is <a href="https://github.com/richtaur/richtaur.com">open source</a>.
+	The design is dynamically updated based on the weather and time of day where I live.
+	More info can be found in the <a href="https://github.com/richtaur/richtaur.com/blob/master/README.md">README</a> file.
+</p>
+
+<p class="sep">
+	I can be reached at <a href="mailto:richtaur@gmail.com">richtaur@gmail.com</a>.
+</p>
+						</div><!-- /.post -->
+<!-- end content -->
+
+		</div></div>
+		<div><div>
+
+					</div>
+				</div><!-- /.bd -->
+			</div>
+		</div><!-- /.module (first in primary) -->
+
+	</div><!-- /#column-primary -->
+
+	<div id="column-secondary">
+
+		<div class="module" id="mod-ldg">
+			<div>
+				<div class="hd">
+					<ul class="btns">
+						<li><button class="close" tooltip="Close this module"><span>close</span></button></li>
+						<li><button class="toggle" tooltip="Toggle this module's contents"><span>toggle content</span></button></li>
+						<li><button class="maximize" tooltip="Maximize this module"><span>maximize</span></button></li>
+					</ul>
+					<h2>Play my game!</h2>
+				</div>
+				<div class="bd">
+					<div>
+						<p>
+							<a class="img-con" href="http://play.lostdecadegames.com/onslaught_arena/demo">
+								<img alt="Onslaught! Arena" src="img/onslaught_arena.png">
+							</a>
+							Play the
+							<a href="http://play.lostdecadegames.com/onslaught_arena/demo">demo</a>
+							of my first game
+							<a href="http://www.lostdecadegames.com/onslaught_arena">Onslaught! Arena</a>
+							in your browser for free.
+						</p>
+						<p>
+							You can read about my wacky adventures in game development on the
+							often-updated
+							<a href="http://blog.lostdecadegames.com/">Lost Decade Games blog</a>.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div><!-- /.module -->
+
+		<iframe
+			id="raptr-badge"
+			frameborder="0"
+			src="http://raptr.com/badges/last-most/richtaur"
+			scrolling="no"
+		></iframe>
+
+	</div><!-- /#column-secondary -->
+
+</div><!-- /#bd -->
+
+<div id="ft">
+	<div class="con">
+		<em>Original content &copy; <?php echo date('Y'); ?> Matt Hackett</em>
+	</div>
+</div>
+
+<!-- semantic, YES? :( -->
+<div id="extra-1"></div>
+<div id="extra-2"></div>
+
+<script src="js/richtaur.js"></script>
+<script>
+richtaur.setPlaying(<?php echo file_get_contents('js/now_playing.json'); ?>);
+richtaur.setWeather(<?php echo file_get_contents('js/yweather.json'); ?>);
+</script>
+
+</body>
+</html>
