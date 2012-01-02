508392cf5a1667c725558bd5a874e8bc8aadc512
Matt Hackett <richtaur@gmail.com>
2012-01-01 21:04:14 -0800

    fixed a js error, added google analytics, using hosted jquery

--- htdocs/index.php
+++ htdocs/index.php
@@ -159,11 +159,28 @@
 <div id="extra-1"></div>
 <div id="extra-2"></div>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script src="js/richtaur.js"></script>
 <script>
 richtaur.setPlaying(<?php echo file_get_contents('js/now_playing.json'); ?>);
 richtaur.setWeather(<?php echo file_get_contents('js/yweather.json'); ?>);
 </script>
+
+<!-- google analytics -->
+<script type="text/javascript">
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-27284870-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+</script>
+<!-- /google analytics -->
 
 </body>
 </html>
