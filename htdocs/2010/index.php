d114741c1475856dc23b91e73aec0631d2cdecbe
Matt Hackett <richtaur@gmail.com>
2012-03-25 16:48:35 -0700

    design implemented

---
+++ htdocs/2010/index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html class="day" lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Hey guys it's Matt Hackett!</title>
+	<link rel="icon" href="img/favicon.gif" type="image/ico">
+	<link rel="openid.delegate" href="http://richtaur.myopenid.com/">
+	<link rel="openid.server" href="http://www.myopenid.com/server">
+	<link rel="shortcut icon" href="img/favicon.gif">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<meta name="description" content="Matt Hackett's website. He's an HTML5 game developer and stuff.">
+	<meta name="keywords" content="richtaur, matt hackett, game developer, silicon valley, video games, foosball, sharks, yahoo, raptr, lost decade games, retro, onslaught!, html5, javascript">
+</head>
+<body>
+
+<div class="con" id="hd">
+	<h1>
+		<a href="/" title="Now you're playing with richtaur">richtaur</a>
+	</h1>
+	<div id="avatar">
+		<div></div>
+	</div>
+</div>
+
+<div id="divider">
+	<div class="con">
+		<ul class="btns">
+			<li><button class="close" tooltip="Browser slowing down? Toggle all animations here"><span>close</span></button></li>
+			<li><button class="disabled"><span>TBD</span></button></li>
+			<li><button class="toggle" tooltip="Toggle ALL content on the page"><span>toggle content</span></button></li>
+		</ul>
+	</div>
+</div><!-- /#divider -->
+
+<div class="con" id="bd">
+
+	<div id="column-primary">
+
+		<div class="module">
+			<div>
+				<div class="hd">
+					<ul class="btns">
+						<li><button class="close" tooltip="Close this module"><span>close</span></button></li>
+						<li><button class="toggle" tooltip="Toggle this module's contents"><span>toggle content</span></button></li>
+						<li><button class="maximize" tooltip="Maximize this module"><span>maximize</span></button></li>
+					</ul>
+					<h2>Welcome</h2>
+				</div>
+				<div class="bd">
+					<div>
+
+<!-- begin content (copypasta from Word Press) -->
+<div class="post">
+
+<p class="primary">
+	My name's <strong>Matt Hackett</strong> and this is my personal website. Here are some hopefully interesting things about me, in a descendingly less interesting order:
+</p>
+
+<br>
+<img alt="Me playing foos" class="after" src="img/about_foos.jpg">
+
+<ol class="list-detailed">
+	<li>
+		I'm <strong>co-founder</strong> of
+		<a href="http://www.lostdecadegames.com/">Lost Decade Games</a>,
+		an independent game company where I work on <strong>HTML5 games</strong> fulltime.
+	</li>
+	<li>
+		I was a <strong>web developer</strong> from 2003 to 2011, working at places like
+		<a href="http://www.yahoo.com/">Yahoo!</a> and
+		<a href="http://raptr.com/">Raptr</a>.
+	</li>
+	<li>I contributed <strong>60 seconds worth of music</strong> to Capcom's <a href="http://www.ocremix.org/info/OC_ReMix:_Super_Street_Fighter_II_Turbo_HD_Remix_Official_Soundtrack">Street Fighter 2 HD Remix</a>.</li>
+	<li>I've written one <strong>professional game review</strong> (of <a href="http://www.gamesradar.com/ds/the-dark-spire/review/the-dark-spire/a-20090518145357896028/g-20081218172959531054">Dark Spire</a> for DS).</li>
+	<li>In 2009, <a href="http://www.flickr.com/photos/matthackett/3488395022/">three friends and I</a> won first place at a <a href="http://www.vifa.com/">VIFA</a>-recognized foosball tournament.</li>
+	<li>In my teens I was cast as an extra in a painfully bad movie called <a href="http://www.imdb.com/title/tt0297024/">Betaville</a>.</li>
+	<li>
+		I graduated from <a href="http://www.siu.edu/">Southern Illinois University</a>
+		with a <strong>Bachelor of Arts in Mass Communication</strong>
+		(and a minor in <strong>Marketing</strong>).
+	</li>
+	<li>I was born the day <a href="http://en.wikipedia.org/wiki/John_Lennon">John Lennon</a> died.</li>
+</ol>
+
+<p class="primary">
+	This website is <a href="https://github.com/richtaur/richtaur.com">open source</a>.
+	The design is dynamically updated based on the weather and time of day where I live.
+	More info can be found in the <a href="https://github.com/richtaur/richtaur.com/blob/master/README.md">README</a> file.
+</p>
+
+<p class="sep">
+	I can be reached at <a href="mailto:richtaur@gmail.com">richtaur@gmail.com</a>.
+</p>
+						</div><!-- /.post -->
+<!-- end content -->
+
+		</div></div>
+		<div><div>
+
+					</div>
+				</div><!-- /.bd -->
+			</div>
+		</div><!-- /.module (first in primary) -->
+
+	</div><!-- /#column-primary -->
+
+	<div id="column-secondary">
+
+		<div class="module" id="mod-ldg">
+			<div>
+				<div class="hd">
+					<ul class="btns">
+						<li><button class="close" tooltip="Close this module"><span>close</span></button></li>
+						<li><button class="toggle" tooltip="Toggle this module's contents"><span>toggle content</span></button></li>
+						<li><button class="maximize" tooltip="Maximize this module"><span>maximize</span></button></li>
+					</ul>
+					<h2>Play my game!</h2>
+				</div>
+				<div class="bd">
+					<div>
+						<p>
+							<a class="img-con" href="http://play.lostdecadegames.com/onslaught_arena/">
+								<img alt="Onslaught! Arena" src="img/onslaught_arena.png">
+							</a>
+							<a href="http://play.lostdecadegames.com/onslaught_arena/">Play</a>
+							my first game
+							<a href="http://www.lostdecadegames.com/onslaught_arena">Onslaught! Arena</a>
+							in your browser for free.
+						</p>
+						<p>
+							You can read about my wacky adventures in indie-HTML5 game development on the
+							<a href="http://blog.lostdecadegames.com/">Lost Decade Games blog</a>.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div><!-- /.module -->
+
+		<iframe
+			id="raptr-badge"
+			frameborder="0"
+			src="http://raptr.com/badges/last-most/richtaur"
+			scrolling="no"
+		></iframe>
+
+	</div><!-- /#column-secondary -->
+
+</div><!-- /#bd -->
+
+<div id="ft">
+	<div class="con">
+		<em>Original content &copy; <?php echo date('Y'); ?> Matt Hackett</em>
+	</div>
+</div>
+
+<!-- semantic, YES? :( -->
+<div id="extra-1"></div>
+<div id="extra-2"></div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="js/richtaur.js"></script>
+<script>
+richtaur.setPlaying(<?php echo file_get_contents('js/now_playing.json'); ?>);
+richtaur.setWeather(<?php echo file_get_contents('js/yweather.json'); ?>);
+</script>
+
+<!-- google analytics -->
+<script type="text/javascript">
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-27284870-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+</script>
+<!-- /google analytics -->
+
+</body>
+</html>
--- htdocs/index.php
+++ htdocs/index.php
@@ -1,170 +1,62 @@
 <!DOCTYPE html>
-<html class="day" lang="en">
+<html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Hey guys it's Matt Hackett!</title>
+	<title>Related Searches</title>
 	<link rel="icon" href="img/favicon.gif" type="image/ico">
 	<link rel="openid.delegate" href="http://richtaur.myopenid.com/">
 	<link rel="openid.server" href="http://www.myopenid.com/server">
 	<link rel="shortcut icon" href="img/favicon.gif">
-	<link rel="stylesheet" href="css/style.css" type="text/css">
-	<meta name="description" content="Matt Hackett's website. He's an HTML5 game developer and stuff.">
-	<meta name="keywords" content="richtaur, matt hackett, game developer, silicon valley, video games, foosball, sharks, yahoo, raptr, lost decade games, retro, onslaught!, html5, javascript">
+	<link href="css/related_searches.css" rel="stylesheet" type="text/css">
+	<meta name="description" content="Matt Hackett, HTML5 game developer: Related Searches">
+	<meta name="keywords" content="richtaur, matt hackett, lost decade games, html5 games, javascript, indie">
 </head>
 <body>
 
-<div class="con" id="hd">
-	<h1>
-		<a href="/" title="Now you're playing with richtaur">richtaur</a>
-	</h1>
-	<div id="avatar">
-		<div></div>
-	</div>
+<div id="hd">
+	<h1>Welcome to richtaur.com</h1>
 </div>
 
-<div id="divider">
-	<div class="con">
-		<ul class="btns">
-			<li><button class="close" tooltip="Browser slowing down? Toggle all animations here"><span>close</span></button></li>
-			<li><button class="disabled"><span>TBD</span></button></li>
-			<li><button class="toggle" tooltip="Toggle ALL content on the page"><span>toggle content</span></button></li>
-		</ul>
-	</div>
-</div><!-- /#divider -->
+<div id="bd">
+	<h2><span>Related Searches</span></h2>
+	<div id="stage">Loading…</div>
+	<ul>
+		<li><a href="mailto:richtaur@gmail.com">Contact</a></li>
+		<li><a href="https://twitter.com/#!/richtaur">Twitter</a></li>
+		<li><a href="http://www.ci.mtnview.ca.us/">Local Weather</a></li>
+		<li><a href="http://www.lostdecadegames.com/">Lost Decade Games</a></li>
+		<li><a href="http://raptr.com/">Web Development</a></li>
+		<li><a href="http://www.gamesradar.com/ds/the-dark-spire/review/the-dark-spire/a-20090518145357896028/g-20081218172959531054">Dark Spire (DS) Review</a></li>
+		<li><a href="http://www.flickr.com/photos/matthackett/3488395022/">Foosball Tournament</a></li>
+		<li><a href="http://www.siu.edu/">Mass Communication</a></li>
+		<li><a href="http://www.ocremix.org/info/OC_ReMix:_Super_Street_Fighter_II_Turbo_HD_Remix_Official_Soundtrack">Street Fighter 2 HD Remix</a></li>
+		<li><a href="http://www.andreaabney.com/">Stinky Beef</a></li>
+		<li><a href="http://marylhackett.com/">Awesome Mom</a></li>
+	</ul>
 
-<div class="con" id="bd">
-
-	<div id="column-primary">
-
-		<div class="module">
-			<div>
-				<div class="hd">
-					<ul class="btns">
-						<li><button class="close" tooltip="Close this module"><span>close</span></button></li>
-						<li><button class="toggle" tooltip="Toggle this module's contents"><span>toggle content</span></button></li>
-						<li><button class="maximize" tooltip="Maximize this module"><span>maximize</span></button></li>
-					</ul>
-					<h2>Welcome</h2>
-				</div>
-				<div class="bd">
-					<div>
-
-<!-- begin content (copypasta from Word Press) -->
-<div class="post">
-
-<p class="primary">
-	My name's <strong>Matt Hackett</strong> and this is my personal website. Here are some hopefully interesting things about me, in a descendingly less interesting order:
-</p>
-
-<br>
-<img alt="Me playing foos" class="after" src="img/about_foos.jpg">
-
-<ol class="list-detailed">
-	<li>
-		I'm <strong>co-founder</strong> of
-		<a href="http://www.lostdecadegames.com/">Lost Decade Games</a>,
-		an independent game company where I work on <strong>HTML5 games</strong> fulltime.
-	</li>
-	<li>
-		I was a <strong>web developer</strong> from 2003 to 2011, working at places like
-		<a href="http://www.yahoo.com/">Yahoo!</a> and
-		<a href="http://raptr.com/">Raptr</a>.
-	</li>
-	<li>I contributed <strong>60 seconds worth of music</strong> to Capcom's <a href="http://www.ocremix.org/info/OC_ReMix:_Super_Street_Fighter_II_Turbo_HD_Remix_Official_Soundtrack">Street Fighter 2 HD Remix</a>.</li>
-	<li>I've written one <strong>professional game review</strong> (of <a href="http://www.gamesradar.com/ds/the-dark-spire/review/the-dark-spire/a-20090518145357896028/g-20081218172959531054">Dark Spire</a> for DS).</li>
-	<li>In 2009, <a href="http://www.flickr.com/photos/matthackett/3488395022/">three friends and I</a> won first place at a <a href="http://www.vifa.com/">VIFA</a>-recognized foosball tournament.</li>
-	<li>In my teens I was cast as an extra in a painfully bad movie called <a href="http://www.imdb.com/title/tt0297024/">Betaville</a>.</li>
-	<li>
-		I graduated from <a href="http://www.siu.edu/">Southern Illinois University</a>
-		with a <strong>Bachelor of Arts in Mass Communication</strong>
-		(and a minor in <strong>Marketing</strong>).
-	</li>
-	<li>I was born the day <a href="http://en.wikipedia.org/wiki/John_Lennon">John Lennon</a> died.</li>
-</ol>
-
-<p class="primary">
-	This website is <a href="https://github.com/richtaur/richtaur.com">open source</a>.
-	The design is dynamically updated based on the weather and time of day where I live.
-	More info can be found in the <a href="https://github.com/richtaur/richtaur.com/blob/master/README.md">README</a> file.
-</p>
-
-<p class="sep">
-	I can be reached at <a href="mailto:richtaur@gmail.com">richtaur@gmail.com</a>.
-</p>
-						</div><!-- /.post -->
-<!-- end content -->
-
-		</div></div>
-		<div><div>
-
-					</div>
-				</div><!-- /.bd -->
-			</div>
-		</div><!-- /.module (first in primary) -->
-
-	</div><!-- /#column-primary -->
-
-	<div id="column-secondary">
-
-		<div class="module" id="mod-ldg">
-			<div>
-				<div class="hd">
-					<ul class="btns">
-						<li><button class="close" tooltip="Close this module"><span>close</span></button></li>
-						<li><button class="toggle" tooltip="Toggle this module's contents"><span>toggle content</span></button></li>
-						<li><button class="maximize" tooltip="Maximize this module"><span>maximize</span></button></li>
-					</ul>
-					<h2>Play my game!</h2>
-				</div>
-				<div class="bd">
-					<div>
-						<p>
-							<a class="img-con" href="http://play.lostdecadegames.com/onslaught_arena/">
-								<img alt="Onslaught! Arena" src="img/onslaught_arena.png">
-							</a>
-							<a href="http://play.lostdecadegames.com/onslaught_arena/">Play</a>
-							my first game
-							<a href="http://www.lostdecadegames.com/onslaught_arena">Onslaught! Arena</a>
-							in your browser for free.
-						</p>
-						<p>
-							You can read about my wacky adventures in indie-HTML5 game development on the
-							<a href="http://blog.lostdecadegames.com/">Lost Decade Games blog</a>.
-						</p>
-					</div>
-				</div>
-			</div>
-		</div><!-- /.module -->
-
-		<iframe
-			id="raptr-badge"
-			frameborder="0"
-			src="http://raptr.com/badges/last-most/richtaur"
-			scrolling="no"
-		></iframe>
-
-	</div><!-- /#column-secondary -->
-
-</div><!-- /#bd -->
+	<form>
+		<input type="text">
+		<input type="submit" value="Search">
+	</form>
+</div>
 
 <div id="ft">
-	<div class="con">
-		<em>Original content &copy; <?php echo date('Y'); ?> Matt Hackett</em>
-	</div>
+	<strong>Related Searches:</strong>
+	<ul>
+		<li><a href="https://twitter.com/#!/geoffblair">Geoff Blair</a></li>
+		<li><a href="http://jmflava.com/">Joshua Morse</a></li>
+		<li><a href="http://www.html5gamedevs.com/">HTML5 Game Developers</a></li>
+	</ul>
 </div>
 
-<!-- semantic, YES? :( -->
-<div id="extra-1"></div>
-<div id="extra-2"></div>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script src="js/richtaur.js"></script>
+<!--
 <script>
 richtaur.setPlaying(<?php echo file_get_contents('js/now_playing.json'); ?>);
 richtaur.setWeather(<?php echo file_get_contents('js/yweather.json'); ?>);
 </script>
+-->
 
-<!-- google analytics -->
+<!-- Google Analytics -->
 <script type="text/javascript">
 
 var _gaq = _gaq || [];
@@ -178,7 +70,7 @@ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga
 })();
 
 </script>
-<!-- /google analytics -->
+<!-- /Google Analytics -->
 
 </body>
 </html>
