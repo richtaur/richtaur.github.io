8015f8cf20fea67878806093a38d5e14c7afab92
Matt Hackett <richtaur@gmail.com>
2012-03-26 22:06:01 -0700

    wrappin' it up for now

--- README.md
+++ README.md
@@ -7,27 +7,7 @@ If you're interested in my work please follow my indie game company [Lost Decade
 ## TODO
 
 * BUG: robust-ify the interaction stuff (easily breaks ATM)
-* Photoshop chick out of photo
-* Make custom background that's bigger and convincing
-* Create icons and buttons
 * Implement weather
-* Create text-based adventure game Search
-
-### Local Weather (HARD)
-
-Provide weather controls!
-
-1. Click
-2. Weather turns on, camera pans to the right where there are weather controls
-3. There's a cancel button which pans the camera back to the left
-
-#### Search
-
-Text-based adventure game
-
-1. Begin typing in search input
-2. Fade out image...
-
-You are in a place.
-examine
-I don't understand that.
+	- find out what API can do
+	- make the thing do it
+* Cancel Search form for now
--- htdocs/index.php
+++ htdocs/index.php
@@ -26,10 +26,9 @@
 	<ul>
 		<!-- Personal -->
 		<li><a href="https://twitter.com/#!/richtaur">@richtaur</a></li>
+		<!--
 		<li><a href="http://www.ci.mtnview.ca.us/">Local Weather</a></li>
-		<li data-image-src="http://farm4.staticflickr.com/3360/3488395022_60e972b46d.jpg">
-			<a href="http://www.flickr.com/photos/matthackett/3488395022/">Foosball Tournament</a>
-		</li>
+		-->
 		<li><a href="http://www.andreaabney.com/">Stinky Beef</a></li>
 		<li data-image-src="http://farm3.staticflickr.com/2652/3872442947_6cae57e1b5.jpg">
 			<a href="http://www.flickr.com/photos/matthackett/3872442947/in/photostream">This</a>
@@ -37,21 +36,25 @@
 		<li data-image-src="https://p.twimg.com/AnAcYGOCMAE1MQj.jpg">
 			<a href="https://twitter.com/#!/richtaur/media/slideshow?url=pic.twitter.com%2Fg40gnj6L">That</a>
 		</li>
+		<li data-image-src="http://farm4.staticflickr.com/3360/3488395022_60e972b46d.jpg">
+			<a href="http://www.flickr.com/photos/matthackett/3488395022/">Foosball Tournament</a>
+		</li>
 
 		<!-- "Professional" -->
+		<li><a href="https://github.com/richtaur">GitHub</a></li>
 		<li>
 			<a href="http://playbombada.appspot.com/static/app.htm">Play Bombada</a>
 		</li>
 		<li data-iframe-src="http://m.lostdecadegames.com/onslaught_defense/">
 			<a href="http://m.lostdecadegames.com/onslaught_defense/">Play Onslaught! Defense</a>
 		</li>
-		<li><a href="https://github.com/richtaur">GitHub</a></li>
 		<li><a href="http://www.lostdecadegames.com/">Lost Decade Games</a></li>
+		<li><a href="http://www.lostdecadegames.com/lostcast/">Lostcast: HTML5 Games Podcast</a></li>
 		<li><a href="2010/">2010 Version</a></li>
 	</ul>
 
-	<form>
-		<input type="text">
+	<form action="https://www.google.com/search">
+		<input name="q" type="text">
 		<input src="img/button.gif" type="image" alt="If you happen to be searching for a very small text-based adventure game, you're in a very specific type of luck!">
 	</form>
 </div>
@@ -88,9 +91,10 @@ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga
 </script>
 <!-- /Google Analytics -->
 
+<!--
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script src="js/related_searches.js"></script>
-<script src="js/search.js"></script>
+-->
 
 </body>
 </html>
