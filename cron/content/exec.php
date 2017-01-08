084252b72eb9516b3121533dced37b44ac362eed
Matt Hackett <richtaur@gmail.com>
2017-01-08 11:08:22 -0800

    -cron

--- cron/content/exec.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/php
-<?php
-
-require('content.php');
-
-$file = $argv[1];
-$url = $argv[2];
-$content = new Content($url, $file);
-$content->write();
--- cron/rss/exec.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/php
-<?php
-
-require('rss.php');
-
-$file = $argv[1];
-$url = $argv[2];
-$rss = new RSS($url, $file);
-$rss->write();
