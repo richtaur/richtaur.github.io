94f2cbc99602a8b6711b9ae993a3517bea3715b9
Matt Hackett <richtaur@gmail.com>
2021-10-07 11:10:22 -0700

    random subtitles why not

---
+++ src/richtaur.js
@@ -0,0 +1,15 @@
+// Get a random subtitle
+const subtitles = [
+	"game developer",
+	"let's make games",
+	"the galaxy is at peace",
+	"words about games",
+	"richtaur.com",
+	"rik tor",
+	"prior podcaster",
+];
+const choice = subtitles[Math.floor(Math.random() * subtitles.length)];
+
+// Show the subtitle
+const subtitle = document.getElementById("subtitle");
+subtitle.innerHTML = choice;
