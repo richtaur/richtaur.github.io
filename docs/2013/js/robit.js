bdb2da93637f1eb053ee980e696812e600378928
Matt Hackett <richtaur@gmail.com>
2023-12-16 11:17:18 -0800

    ok let's just try adding docs to version control

---
+++ docs/2013/js/robit.js
@@ -0,0 +1,17 @@
+var robit = (function () {
+
+	var game = {
+		home: "You awake suddenly. It's cold. You don't know where you are or how you got here."
+	};
+
+	var map = [
+		[],
+	];
+
+	return {
+		startNewGame: function () {
+			return "";
+		}
+	};
+
+})();
